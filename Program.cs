ab1a73b7c1f81b3a1d58af64b46610aa0af9ddda
Robert Bizon <m4stersplint3r@gmail.com>
2020-10-21 00:23:29 -0400

    lab complete

--- Program.cs
+++ Program.cs
@@ -7,90 +7,37 @@ namespace RegularExpressions
     {
         static void Main(string[] args)
         {
-            string name = "Bobby";
-            if (IsNameValid(name))
-            {
-                Console.WriteLine($"{name} is a valid name.");
-            }
-            else
-            {
-                Console.WriteLine($"{name} is not a valid name.");
-            }
-            string name2 = "bobby";
-            if (IsNameValid(name2))
-            {
-                Console.WriteLine($"{name2} is a valid name.");
-            }
-            else
-            {
-                Console.WriteLine($"{name2} is not a valid name.");
-            }
+            string name, email, number, date, HTML;
+            Console.Write("Please enter a valid name: ");
+            name = Console.ReadLine();
+            IsNameValid(name);            
 
-            string email = "Bobby12@gmail.com";
-            if (IsEmailValid(email))
-            {
-                Console.WriteLine($"{email} is a valid email.");
-            }
-            else
-            {
-                Console.WriteLine($"{email} is not a valid email.");
-            }
-            string email2 = "Bobby12@gml.c";
-            if (IsEmailValid(email2))
-            {
-                Console.WriteLine($"{email2} is a valid email.");
-            }
-            else
-            {
-                Console.WriteLine($"{email2} is not a valid email.");
-            }
+            Console.Write("Please enter a valid email: ");
+            email = Console.ReadLine();
+            IsEmailValid(email);
 
-            string number = "734-678-2431";
-            if (IsPhoneNumValid(number))
-            {
-                Console.WriteLine($"{number} is a valid phone number.");
-            }
-            else
-            {
-                Console.WriteLine($"{number} is not a valid phone number.");
-            }
-            string number2 = "7341-678-2431";
-            if (IsPhoneNumValid(number2))
-            {
-                Console.WriteLine($"{number2} is a valid phone number.");
-            }
-            else
-            {
-                Console.WriteLine($"{number2} is not a valid phone number.");
-            }
+            Console.Write("Please enter a valid phone number: ");
+            number = Console.ReadLine();
+            IsPhoneNumValid(number);
 
-            string HTML = "<p> some text </p>";
-            if (IsValidHTML(HTML))
-            {
-                Console.WriteLine($"{HTML} is a valid HTML tag.");
-            }
-            else
-            {
-                Console.WriteLine($"{HTML} is not a valid HTML tag.");
-            }
-            string HTML2 = "<p> some text <p>";
-            if (IsValidHTML(HTML2))
-            {
-                Console.WriteLine($"{HTML2} is a valid HTML tag.");
-            }
-            else
-            {
-                Console.WriteLine($"{HTML2} is not a valid HTML tag.");
-            }
+            Console.Write("Please enter a valid date: ");
+            date = Console.ReadLine();
+            IsDateValid(date);
+            
+            Console.Write("Please enter a valid HTML: ");
+            HTML = Console.ReadLine();
+            IsHTMLValid(HTML);
         }
         static bool IsNameValid(string name)
         {
             if (Regex.IsMatch(name.Trim(), "^[A-Z]{1}[A-Za-z]+$"))
             {
+                Console.WriteLine($"{name} is a valid name.{Environment.NewLine}");
                 return true;
             }
             else
             {
+                Console.WriteLine($"{name} is not a valid name.{Environment.NewLine}");
                 return false;
             }
         }
@@ -99,10 +46,12 @@ namespace RegularExpressions
             // 
             if (Regex.IsMatch(email.Trim(), "[A-z0-9]{5,30}@[A-z0-9]{5,10}.[A-z0-9]{2,3}"))
             {
+                Console.WriteLine($"{email} is a valid email.{Environment.NewLine}");
                 return true;
             }
             else
             {
+                Console.WriteLine($"{email} is not a valid email.{Environment.NewLine}");
                 return false;
             }
         }
@@ -111,22 +60,39 @@ namespace RegularExpressions
             if (Regex.IsMatch(num.Trim(), "^[0-9]{3}-{1}[0-9]{3}-{1}[0-9]{4}$"))
 
             {
+                Console.WriteLine($"{num} is a valid phone number.{Environment.NewLine}");
                 return true;
             }
             else
             {
+                Console.WriteLine($"{num} is not a valid phone number.{Environment.NewLine}");
                 return false;
             }
         }
-        static bool IsValidHTML(string code)
+        static bool IsHTMLValid(string code)
         {
             // <p> </p>
             if (Regex.IsMatch(code.Trim(), "^([<]{1}[A-z]+[>]{1}).*([<]{1}[/]{1}[A-z]+[>]{1})$"))
             {
+                Console.WriteLine($"{code} is a valid HTML tag.{Environment.NewLine}");
                 return true;
             }
             else
             {
+                Console.WriteLine($"{code} is not a valid HTML tag.{Environment.NewLine}");
+                return false;
+            }
+        }
+        static bool IsDateValid(string date)
+        {
+            if (Regex.IsMatch(date.Trim(), "(([0-2][0-9])|([3][01])){1}[/]{1}((0[0-9])|(1[0-2])){1}[/]{1}([0-9][0-9][0-9][0-9]){1}"))
+            {
+                Console.WriteLine($"{date} is a valid date.{Environment.NewLine}");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine($"{date} is not a valid date.{Environment.NewLine}");
                 return false;
             }
         }
